c33ab51fe1bb8999f5ef5441dcde71f2b3f45651
Timo Heister <timo.heister@gmail.com>
2015-01-05 14:11:29 -0500

    test gcc, petsc, clang

--- test.sh
+++ test.sh
@@ -5,6 +5,36 @@ submit="OFF"
 sha=$1
 name=$2
 
+run_gcc()
+{
+desc=$1
+logfile=$2
+submit=$3
+cmake -G "Ninja" ../aspect >$logfile 2>&1
+nice ninja >>$logfile 2>&1
+nice ctest -S ../aspect/tests/run_testsuite.cmake -DDESCRIPTION="$desc" -Dsubmit=$submit -V -j 4 >>$logfile 2>&1
+}
+
+run_gccpetsc()
+{
+desc=$1
+logfile=$2
+submit=$3
+cmake -G "Ninja" -D ASPECT_USE_PETSC=ON ../aspect >$logfile 2>&1
+nice ninja >>$logfile 2>&1
+nice ctest -S ../aspect/tests/run_testsuite.cmake -DDESCRIPTION="$desc" -Dsubmit=$submit -V -j 4 >>$logfile 2>&1
+}
+
+run_clang()
+{
+desc=$1
+logfile=$2
+submit=$3
+cmake -D DEAL_II_DIR=/root/deal.II/installedclang -G "Ninja" ../aspect >$logfile 2>&1
+clang nice ninja >>$logfile 2>&1
+nice ctest -S ../aspect/tests/run_testsuite.cmake -DDESCRIPTION="$build$name" -Dsubmit=$submit -V -j 4 >>$logfile 2>&1
+}
+
 output() {
 basepath=$1
 build=$2
@@ -20,9 +50,9 @@ rm -rf $DIR
 mkdir $DIR
 cd $DIR
 echo hi from `pwd`
-cmake -G "Ninja" ../aspect >$logfile 2>&1
-nice ninja >>$logfile 2>&1
-nice ctest -S ../aspect/tests/run_testsuite.cmake -DDESCRIPTION="$build$name" -Dsubmit=$submit -V -j 4 >>$logfile 2>&1
+
+eval run_$build $build$name $logfile $submit
+
 grep "Compiler errors" $logfile >>$summary
 #grep "Compiler warnings" $logfile >>$summary
 #if [ "$build" != "clang" ]
@@ -36,15 +66,13 @@ basepath=`pwd`
 mkdir -p $basepath/logs/$sha
 rm -f $basepath/logs/$sha/*
 
-build="clang"
-#output $basepath $build $sha $name
 
-build="gcc"
+# do the testing:
+for build in "clang" "gcc" "gccpetsc"; do
 output $basepath $build $sha $name
+done
 
-build="gccpetsc"
-#output $basepath $build $sha $name
-
+# manual:
 (
 cd $basepath/aspect/doc/ && exit 0 &&
 make manual.pdf >/dev/null 2>&1 &&
