47b4e3c6f215603d5235ff9b99a6cf6e183a8b54
Timo Heister <timo.heister@gmail.com>
2015-08-08 10:32:25 -0500

    allow testing old versions

---
+++ old.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# todo: need to clean deal.II build directory
+
+# Run some selected old versions (releases) and apply necessary patches before
+# doing so.
+
+cd dealii;git checkout v8.3.0;cd ..
+./test.sh
+
+cd dealii;git checkout v8.2.1;cd ..
+patch -R -p3 <patches/821andbefore.patch
+./test.sh
+
+# not tested:
+# not working because dealii/deal.II directory
+cd dealii;git checkout v8.1.0;cd ..
+./test.sh
+
+# not tested:
+cd dealii;git checkout v8.0.0;cd ..
+./test.sh 
