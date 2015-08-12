116af369c9398f847ca067fa52ca849ab4909e3a
Timo Heister <timo.heister@gmail.com>
2015-08-12 11:51:15 +0800

    update configuration

--- config.py
+++ config.py
@@ -8,16 +8,16 @@ if os.path.isfile('token.txt'):
     token = open('token.txt').read().replace("\n","")
 
 # directory where the aspect git repo sits
-repodir = os.getcwd()+"/aspect"
+repodir = os.getcwd()+"/dealii"
 
 # name of the user/organization on github
-github_user = "geodynamics"
+github_user = "dealii"
 
 # name of the repo on github
-github_repo = "aspect"
+github_repo = "dealii"
 
 # number of tests to go back in time when doing "run-all" default 10
-n_old_tests = 10
+n_old_tests = 3 #10
 
 # if a user that is_allowed() posts a comment that has
 # has_hotword(text) return True, the PR is tested
@@ -37,7 +37,7 @@ def is_allowed(username):
 
 # make a nice link for a test result:
 def make_link(sha):
-    return "http://www.math.clemson.edu/~heister/aspect-logs/{}/".format(sha)
+    return "nolink"#http://www.math.clemson.edu/~heister/aspect-logs/{}/".format(sha)
 
 
 # for a given line l of the test output return
