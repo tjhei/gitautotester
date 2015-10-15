7cd601d0e6483f0caf2466e64d493b62c5df4ebf
Timo Heister <timo.heister@gmail.com>
2015-10-15 15:57:09 -0400

    generate changes.diff

--- test.sh
+++ test.sh
@@ -13,6 +13,7 @@ submit=$3
 cmake -G "Ninja" -D ASPECT_RUN_ALL_TESTS=ON ../aspect || { echo "configure FAILED"; return; }
 nice ninja || { echo "build FAILED"; return; }
 nice ctest --output-on-failure -S ../aspect/tests/run_testsuite.cmake -DDESCRIPTION="$desc" -Dsubmit=$submit -V -j 10 || { echo "test FAILED"; return; }
+nice ninja generate_reference_output
 }
 
 run_gccpetsc()
@@ -73,6 +74,9 @@ for build in "clang" "gcc" "gccpetsc"; do
 output $basepath $build $sha $name
 done
 
+# reference output
+cp $basepath/build-gcc/changes.diff $basepath/logs/$sha/ >/dev/null 2>&1
+
 # manual:
 (
 cd $basepath/aspect/doc/ && exit 0 &&
@@ -91,6 +95,10 @@ sed -i 's/\([0-9]*\)% tests passed, \([0-9]*\) tests failed out of \([0-9]*\)/te
 
 sed 's#$# <br/>#' $basepath/logs/$sha/summary > $basepath/logs/$sha/index.html
 sed -i 's#^BUILD \(.*\):#<a href="\1">BUILD \1</a>#' $basepath/logs/$sha/index.html
+
+if [ -s $basepath/build-gcc/changes.diff ]; then
+  echo "diffs: see <a href=\"changes.diff\">changes.diff</a>" >> $basepath/logs/$sha/index.html
+fi
 
 cat $basepath/logs/$sha/summary
 
