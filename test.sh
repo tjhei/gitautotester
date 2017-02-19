6e433c223b4d8778fc6fe673d99e6ded94a290ba
Timo Heister <timo.heister@gmail.com>
2017-02-19 11:28:34 -0500

    test.sh: do not skip releases

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 TESTS="step-22 tablehandler test_assembly test_poisson test_hp"
-TESTS="test_assembly"
+TESTS="test_assembly tablehandler"
 sha=`cd dealii;git rev-parse HEAD`
-desc="`cd dealii;git rev-parse --short HEAD;` `cd dealii;git describe --exact-match HEAD 2>/dev/null`"
+name=`cd dealii;git describe --exact-match HEAD 2>/dev/null`
+desc="`cd dealii;git rev-parse --short HEAD;` $name"
 time=`cd dealii;git show --quiet --format=%cD HEAD | head -n 1`
 basepath=`pwd`
 
@@ -18,18 +19,18 @@ logfile=$basepath/logs/$sha/build
 
 
 # only test sha1 that start with "a":
-if [[ $sha == a* ]];
+if [[ -n "$name" ]] || [[ $sha == a* ]];
 then
   echo "Testing $sha - $desc" | tee $logfile 
 else
-  echo "skipping $sha";
+  echo "skipping $sha - $desc";
   exit 1
 fi
 
 mkdir -p $DIR
 cd $DIR
 echo hi from `pwd` >>$logfile
-cmake -G "Ninja" -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=`pwd`/../install  ../dealii >>$logfile 2>&1 && nice ninja install >>$logfile 2>&1 || exit -1
+cmake -G "Ninja" -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=`pwd`/install  $basepath/dealii >>$logfile 2>&1 && nice ninja install >>$logfile 2>&1 || exit -1
 
 cd ..
 
@@ -37,7 +38,7 @@ for test in $TESTS ; do
   cd $test
   echo "** working on $test" >>$logfile
   rm -fr CMakeCache.txt CMakeFiles Makefile
-  cmake -D DEAL_II_DIR=../install . >>$logfile 2>&1
+  cmake -D DEAL_II_DIR=../build/install . >>$logfile 2>&1
   echo $sha >tmp
   echo $test >>tmp
   echo $desc >>tmp
@@ -45,6 +46,7 @@ for test in $TESTS ; do
   make run 2>>$logfile | grep "^>" >>tmp
   cd ..
   cat $test/tmp | python render.py record >> $basepath/logs/$sha/summary
+  cp $test/tmp $basepath/logs/$sha/$test
 done
 
 cat $basepath/logs/$sha/summary
